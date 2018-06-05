d047a1e0a9285489a2f6298d3274b48e4f72481a
Owen Leong <owenl131@gmail.com>
2018-06-05 11:02:39 +0800

    Add up to q7

--- solutions_001-010.lisp
+++ solutions_001-010.lisp
@@ -5,8 +5,8 @@
     (car list)
     (last-box (cdr list))))
 
-"Q1 - expected: DORA"
-(last-box '(alan barry candy dora))
+(format t "~%Q1 - expected: ~%DORA")
+(print (last-box '(alan barry candy dora)))
 
 ;; Q2 Find the last but one box of a list
 (defun last-but-one (lst)
@@ -14,8 +14,8 @@
     lst
     (last-but-one (cdr lst))))
 
-"Q2 - expected: (CANDY DORA)"
-(last-but-one '(alan barry candy dora))
+(format t "~%Q2 - expected: ~%(CANDY DORA)")
+(print (last-but-one '(alan barry candy dora)))
 
 ;; Q3 - find K'th element of a list (0-indexed)
 (defun find-k (lst k)
@@ -23,8 +23,8 @@
     (car lst)
     (find-k (cdr lst) (- k 1))))
 
-"Q3 - expected: E"
-(find-k '(a b c d e f g) 4)
+(format t "~%Q3 - expected: ~%E")
+(print (find-k '(a b c d e f g) 4))
 
 ;; Q4 Find the number of elements in a list
 (defun list-len (lst)
@@ -32,8 +32,8 @@
     0
     (+ 1 (list-len (cdr lst)))))
 
-"Q4 - expected: 5"
-(list-len '(1 5 2 4 3))
+(format t "~%Q4 - expected: ~%5")
+(print (list-len '(1 5 2 4 3)))
 
 ;; Q5 Reverse a list
 (defun reverse-list (list)
@@ -41,12 +41,27 @@
     '()
     (append (reverse-list (cdr list)) (list (car list)))))
 
-"Q5 - expected: (E D C B A)"
-(reverse-list '(A B C D E))
+(format t "~%Q5 - expected: ~%(E D C B A)")
+(print (reverse-list '(A B C D E)))
 
 ;; Q6 Find out whether a list is a palindrome
+(defun is-palindrome (lst)
+  (equal lst (reverse lst)))
+
+(format t "~%Q6 - expected: ~%T~%NIL")
+(print (is-palindrome '(1 2 3 2 1)))
+(print (is-palindrome '(1 2 3 1 2)))
 
 ;; Q7 Flatten a nested list structure
+(defun flatten-list (lst)
+  (if (equal lst '())
+    '()
+    (if (listp (car lst))
+      (append (flatten-list (car lst)) (flatten-list (cdr lst)))
+      (append (list (car lst)) (flatten-list (cdr lst))))))
+
+(format t "~%Q7 - expected: ~%(A B C D E F G)")
+(print (flatten-list '(a b (c d) (e (f) () (g)))))
 
 ;; Q8 Eliminate consecutive duplicates in a list
 
@@ -56,3 +71,4 @@
 
 ;; Q10 Run-length encoding of a list
 
+(format t "~%")
